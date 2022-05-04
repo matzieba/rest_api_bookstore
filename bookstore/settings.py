1e12ac02c620abe0dc6d077534adf21fe4517e17
Mateusz Zieba <matizieba@gmail.com>
2022-05-04 18:00:19 +0200

    folderreorg

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eg6ye0w(vz7wh)1jbg9apz4f)6^=7fjd^!nc#83zt*u25i51gq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
