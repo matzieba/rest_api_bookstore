a55e75a49f3973cbebf37368a71b80f6a73c2794
Mateusz Zieba <matizieba@gmail.com>
2022-05-04 17:46:45 +0200

    folderreorg

--- py_client/detail.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-endpoint = "http://localhost:8000/api/books/1"
-
-
-get_response = requests.get(endpoint, json = {"title":'kupa'})
-
-print(get_response.json())
--- py_client/list.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-endpoint = "http://localhost:8000/api/books/list"
-
-
-
-get_response = requests.get(endpoint)
-print(get_response.json())
