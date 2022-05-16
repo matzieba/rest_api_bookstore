52b841a44c06f5c5b5d40e17d1445ae61ba5d645
matizieba@gmail.com <matizieba@gmail.com>
2022-05-16 10:03:53 +0200

    settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#os.environ.get("SECRET_KEY")
-SECRET_KEY = 'sajdfahfagjagkakgagag'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['restapibookstore.herokuapp.com', '127.0.0.1', 'localhost']
 
