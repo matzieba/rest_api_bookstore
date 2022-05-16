daa967134269d33d2dd1d8b365011a02e30899cf
matizieba@gmail.com <matizieba@gmail.com>
2022-05-16 11:05:05 +0200

    settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'CONN_MAX_AGE': 500
     }
 }
